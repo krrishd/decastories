cfd6884d4c669c89f69c5878027bf8f87c6d6038
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-04-28 19:31:31 -0600

    got something functional up and running

--- _root/ctrl.js
+++ _root/ctrl.js
@@ -1,57 +1,95 @@
-var app = angular.module('decaStories', ['ngRoute']);
-
-app.config(function($routeProvider, $locationProvider) {
-	$routeProvider
-		.when('/', {
-			templateUrl: 'partials/home.html',
-			controller: homeCtrl
+var app = angular.module('decaStories', ['ui.router', 'snap']);
+var api = 'http://iamdeca.herokuapp.com';
+app.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('home', {
+			url : '/',
+			templateUrl : 'views/home.html',
+			controller : homeCtrl
 		})
-		.when('/submit', {
-			templateUrl: 'partials/submit.html',
-			controller: submitCtrl
+		
+		.state('state', {
+			url : '/state/:state',
+			templateUrl : 'views/state.html',
+			controller : stateCtrl
+		})
+
+		.state('story', {
+			url : '/story/:id',
+			templateUrl : 'views/story.html',
+			controller : storyCtrl
+		})
+
+		.state('new', {
+			url : '/new',
+			templateUrl : 'views/new.html',
+			controller : newCtrl
 		});
+
+		$urlRouterProvider.otherwise('/');
 });
 
-function homeCtrl($scope, $http) {
-	Tabletop.init({
-		key: '0Ah4ub_GLQC5OdExES0hSaHNJRzIzS0FtNzZHdWRQQVE',
-		callback: storeData,
-		simpleSheet: true
+function mainCtrl($scope, $http) {
+	$http.get(api + '/api/states').success(function(data) {
+		$scope.states = data;
 	});
-	
-	function storeData(data, tabletop) {
-		$scope.data = data.reverse();
-		$scope.$apply();
-	}
-	var reloadData = function() {
-		Tabletop.init({
-			key: '0Ah4ub_GLQC5OdExES0hSaHNJRzIzS0FtNzZHdWRQQVE',
-			callback: storeData,
-			simpleSheet: true
-		});
-	
-		function storeData(data, tabletop) {
-			$scope.data = data.reverse();
-			$scope.$apply();
-		}	
-	}
-	setInterval(reloadData(), 30000);
 }
 
-function submitCtrl($scope) {
-	document.querySelector('.submit').addEventListener('click', function(e) {
-		e.preventDefault();
-		if(document.querySelector('textarea').value.length > 800) {
-			var excess = document.querySelector('textarea').value.length - 800;	
-			alert('You have exceeded the word limit by ' + excess + ' words. Try cutting it down a bit, even though you love DECA so much :)');
+function homeCtrl($scope, $http) {
+	$http.get(api + '/api/stories').success(function(data) {
+		$scope.stories = data.reverse();
+	});
+}
+
+function stateCtrl($scope, $http, $stateParams) {
+	$http.get(api + '/api/states').success(function(data) {
+		$scope.states = data;
+	});
+	function findState(abbr) {
+		var theState = "";
+		for(i=0; i<$scope.states.length; i++) {
+			if ($scope.states[i].abbreviation == abbr) {
+				theState = $scope.states[i].name;
+			}
+		};
+		return theState;
+	};
+	$http.get(api + '/api/stories/' + $stateParams.state).success(function(data) {
+		$scope.stories = data.reverse();
+		if (data.length < 1) {
+			alert('No stories yet!');
+			window.location.hash = "#/";
 		}
-		else if(document.querySelector('.title').value.length > 40) {
-			var excess =  document.querySelector('.title').value.length - 40;
-			alert('Your story title exceeds the character limit by ' + excess + ' characters. Shorten it and try again :)');
-		}
-		else {
-			alert('Submitted!');
-			document.querySelector('form').submit();
-		}
+	});	
+}
+
+function storyCtrl($scope, $http, $stateParams) {
+	$scope.storyId = $stateParams.id;
+	$http.get(api + '/api/story/' + $scope.storyId).success(function(data) {
+		$scope.story = data;
+	});
+}
+
+function newCtrl($scope, $http) {	
+	$scope.newStory = function() {
+		$scope.payload = {
+			name : $scope.name,
+			state : $scope.state,
+			chapter : $scope.chapter,
+			title : $scope.title,
+			content : $scope.content
+		};
+		$http.post(api + '/api/stories/new', $scope.payload)
+			.success(function(data) {
+				alert('Posted!');
+				window.location.hash = '#/';
+			})
+			.error(function(err) {
+				alert('Oops! Something went wrong');
+			});
+	};
+
+	$http.get(api + '/api/states').success(function(data) {
+		$scope.states = data;
 	});
 }
